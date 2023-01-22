9fd3f956feb1bbbd939798c7c6ce0196638004d0
darrel.frade <darrelfrade@gmail.com>
2023-01-22 15:28:44 +0900

    Implementing AI from scratch using numpy

--- AIFromScratch/main.py
+++ AIFromScratch/main.py
@@ -2,14 +2,112 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.datasets
 
-input, expectedOutput = sklearn.datasets.make_moons(200, noise = 0.5)
+X,y = sklearn.datasets.make_moons(200, noise = 0.15)
 
 # plot data
-plt.scatter(input[:,0],input[:,1], c=expectedOutput)
+plt.scatter(X[:,0],X[:,1], c=y)
 
 
 input_neurons_length = 2
 output_neurons_length = 2 #cross entrype 2 otherwise1
-number_of_sample = input.shape[0]
+number_of_sample = X.shape[0]
 learning_rate = 0.001;
 regular_expression_constant = 0.01
+
+def retreive(model_dict):
+    W1 = model_dict['W1']
+    b1 = model_dict['b1']
+    W2 = model_dict['W2']
+    b2 = model_dict['b2']
+    return W1, b1, W2, b2
+def forward(x, model_dict):
+    W1, b1, W2, b2 = retreive(model_dict)
+    z1 = X.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    exp_scores = np.exp(z2)
+    softmax = exp_scores / np.sum(exp_scores, axis = 1, keepdims = True)
+    return z1, a1, softmax
+def loss(softmax, y, model_dict):
+    W1, b1, W2, b2 = retreive(model_dict)
+    m = np.zeros(200)
+    for i,correct_index in enumerate(y):
+        predicted = softmax[i][correct_index]
+        m[i] = predicted
+    log_prob = -np.log(m)
+    loss = np.sum(log_prob)
+    reg_loss = regular_expression_constant / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+    loss+= reg_loss
+    return float(loss / y.shape[0])
+def predict(model_dict, x):
+    W1, b1, W2, b2 = retreive(model_dict)
+    z1 = x.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    exp_scores = np.exp(z2)
+    softmax = exp_scores / np.sum(exp_scores, axis = 1, keepdims = True)   # (200,2)
+    return np.argmax(softmax, axis = 1)    # (200,)
+
+
+def backpropagation(x, y, model_dict, epochs):
+    for i in range(epochs):
+        W1, b1, W2, b2 = retreive(model_dict)
+        z1, a1, probs = forward(x, model_dict)  # a1: (200,3), probs: (200,2)
+        delta3 = np.copy(probs)
+        delta3[range(x.shape[0]), y] -= 1  # (200,2)
+        dW2 = (a1.T).dot(delta3)  # (3,2)
+        db2 = np.sum(delta3, axis=0, keepdims=True)  # (1,2)
+        delta2 = delta3.dot(W2.T) * (1 - np.power(np.tanh(z1), 2))
+        dW1 = np.dot(x.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+        # Add regularization terms
+        dW2 += regular_expression_constant * np.sum(W2)
+        dW1 += regular_expression_constant * np.sum(W1)
+        # Update Weights: W = W + (-lr*gradient) = W - lr*gradient
+        W1 += -learning_rate * dW1
+        b1 += -learning_rate * db1
+        W2 += -learning_rate * dW2
+        b2 += -learning_rate * db2
+        # Update the model dictionary
+        model_dict = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+        # Print the loss every 50 epochs
+        if i % 50 == 0:
+            print("Loss at epoch {} is: {:.3f}".format(i, loss(probs, y, model_dict)))
+
+    return model_dict
+
+# Define Initial Weights
+def init_network(input_dim, hidden_dim, output_dim):
+    model = {}
+    # Xavier Initialization
+    w1 = np.random.randn(input_dim, hidden_dim) / np.sqrt(input_dim)
+    b1 = np.zeros((1, hidden_dim))
+    w2 = np.random.randn(hidden_dim, output_dim) / np.sqrt(hidden_dim)
+    b2 = np.zeros((1, output_dim))
+    model['W1'] = w1
+    model['b1'] = b1
+    model['W2'] = w2
+    model['b2'] = b2
+    return model
+def plot_decision_boundary(pred_func):
+    """
+    Code adopted from: https://github.com/dennybritz/nn-from-scratch
+    """
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole gid
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+    plt.title("Decision Boundary for hidden layer size 3")
+
+# Now Let's start the action
+weight_and_bais_dic = init_network(input_dim = input_neurons_length , hidden_dim = 3, output_dim = output_neurons_length)
+model = backpropagation(X, y, weight_and_bais_dic, 1000)
+#plot_decision_boundary(lambda x: predict(model, X))
